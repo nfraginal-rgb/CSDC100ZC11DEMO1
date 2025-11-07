b0d885dc0c19103055ca8a632dd3cda448c3a6f0
nfraginal-rgb <nfraginal@gbox.adnu.edu.ph>
2025-11-07 08:43:40 +0800

    Add files via upload

---
+++ fraginalsample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string FirstName, LastName, FullName;
+	FirstName = "Nicole Anne";
+	LastName = "Fraginal";
+	FullName = FirstName + " " + LastName;
+	cout << FullName;
+	
+	return 0;
+}
